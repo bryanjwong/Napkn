8be45d804922f758a8d7372b66792472f91f04bd
Bryan Wong <bryanjwong@g.ucla.edu>
2020-01-12 01:54:24 -0800

    finalizing mcu progrprogram, updated libraries, improved website

--- app.js
+++ app.js
@@ -12,33 +12,60 @@ ref.once("value", function(snapshot) {
   snapshot.forEach(function(childSnapshot) {
     console.log(childSnapshot.child("DeviceStatus").val());
     let item = document.createElement('li');
-    let tableNum = document.createElement('p');
-    let status = document.createElement('p');
-    let timeLeft = document.createElement('p');
-    
-    // Print out table number
-    tableNum.innerHTML = "Table " + i;
-    item.appendChild(tableNum);
-    tableList.appendChild(item);
-    i++;
-    
-    // Display device status (open or occupied)
-    status.innerHTML = "Status: " + childSnapshot.child("DeviceStatus").val();
-    tableList.appendChild(status);
+    item.classList.add("napkn-num");
 
-    
-    
+    let tableNum = document.createElement('li');
+    tableNum.classList.add("napkn-num");
+
+    // napkin title
+    let div1 = document.createElement('div')
+    div1.classList.add("col-lg-4");
+    div1.innerText = "Napkn #" + i + " ";
+    div1.classList.add("napkn-title");
+    tableList.appendChild(div1);
+
+    let div2 = document.createElement('div')
+    div2.classList.add("col-lg-4");
+    div2.innerText = "Status: " + childSnapshot.child("DeviceStatus").val() + " ";
+    div2.classList.add("napkn-status");
+    tableList.appendChild(div2);
+
+    let div3 = document.createElement('div')
+    div3.classList.add("col-lg-4");
+    div3.classList.add("napkn-timeLeft");
+    tableList.appendChild(div3);
+
+
+    let status = document.createElement('li');
+    status.classList.add("napkn-status");
+    let timeLeft = document.createElement('li');
+    timeLeft.classList.add("napkn-timeLeft");
+
+    let space = document.createElement('div');
+
+    // Print out table number
+    // tableNum.innerHTML = "Napkn #" + i + " ";
+    // item.appendChild(tableNum);
+    // tableList.appendChild(item);
+    i++;
+
+    // Display device status (open or occupied)
+    // status.innerHTML = "Status: " + childSnapshot.child("DeviceStatus").val() + " ";
+    // tableList.appendChild(status);
+
+
+
     // Display minutes remaining
     var statusStr = childSnapshot.child("DeviceStatus").val().toString();
     if (statusStr == "Open"){
-      timeLeft.innerHTML = "Time remaining: N/A";
-      numOpenTables++; 
+      div3.innerText = "Time remaining: N/A ";
+      numOpenTables++;
     }else{ // otherwise, occupied
-      timeLeft.innerHTML = "Time remaining: " + childSnapshot.child("TimeRemaining").val();
+      div3.innerText = "Time remaining: " + childSnapshot.child("TimeRemaining").val();
     }
     tableList.appendChild(timeLeft);
+    tableList.appendChild(space);
   });
 
   openTables.innerHTML = "Tables Open: " + numOpenTables; // Display num open tables
 });
-
