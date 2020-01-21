2ce2452caf94bc7014a4879af5eb426964fed616
Bryan J Wong <bryanjwong@g.ucla.edu>
2020-01-20 17:35:46 -0800

    Update Napkn.ino

--- Napkn/Napkn.ino
+++ Napkn/Napkn.ino
@@ -3,8 +3,8 @@
 // WiFi Libraries
 #include <WiFi.h>                                                   // esp32 library
 #include <IOXhop_FirebaseESP32.h>                                   // firebase library
-#define FIREBASE_HOST "napkn-test-d7a85.firebaseio.com"             // the project name address from firebase id
-#define FIREBASE_AUTH "o1DICNIM0C1pJboj0sFu0VH5mMZ4U9zQZwq8M0jq"    // the secret key generated from firebase
+#define FIREBASE_HOST ""                                            // the project name address from firebase id
+#define FIREBASE_AUTH ""                                            // the secret key generated from firebase
 #define WIFI_SSID "AndroidAP"                                       // input your home or public wifi name
 #define WIFI_PASSWORD "wxai8878"                                    // password of wifi ssid
 //#define WIFI_SSID "UCLA_WEB"                                        // input your home or public wifi name
